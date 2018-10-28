bb6f5964e67399ce7c9152a3f5c433ef6e4d7524
Colin Wee <2gbleh@gmail.com>
2018-10-28 01:44:13 -0400

    convert demo code to file synchronization class

--- sync_file.py
+++ sync_file.py
@@ -1,47 +1,45 @@
-#!/usr/bin/env python3
+##
+# @file sync_file.py
+# @brief Implement syncinc of files over the internet.
+# @author cwee
 
-# for https_get
-from http.client import HTTPSConnection
-from base64 import b64encode
 
-# for https_put
+import base64
 import urllib.request
 
 
-def https_get():
-    #This sets up the https connection
-    c = HTTPSConnection("dav.box.com")
-    #we need to base 64 encode it 
-    #and then decode it to acsii as python 3 stores it as a byte string
-    userAndPass = b64encode(b"user@email.com:password").decode("ascii")
-    headers = { 'Authorization' : 'Basic %s' %  userAndPass }
-    #then connect
-    c.request('GET', '/dav/path/to/hello.txt', headers=headers)
-    #get the response back
-    res = c.getresponse()
-    # at this point you could check the status etc
-    # this gets the page text
-    data = res.read()  
-    print(data)
+class WebDAVFsync:
+    def __init__(self, url, local_fpath, username, password):
+        self.url = url
+        self.local_fpath = local_fpath
+        userpass = "{}:{}".format(username, password).encode()
+        self.auth_b64 = base64.b64encode(userpass).decode("ascii")
 
+    def upload(self):
+        with open(self.local_fpath, "rb") as upload_f:
+            file_data = upload_f.read()
 
-def https_put():
-    with open("go_board.pickle", "rb") as f:
-        file_data = f.read()
+        put_req = urllib.request.Request(
+            url=self.url,
+            data=file_data,
+            method="PUT",
+        )
+        put_req.add_header("Authorization", "Basic {}".format(self.auth_b64))
+        with urllib.request.urlopen(put_req) as url_req:
+            pass
 
-    req = urllib.request.Request(
-        url="https://dav.box.com/dav/path/to/goboard.pickle",
-        data=file_data,
-        method="PUT"
-    )
-    userAndPass = b64encode(b"user@email.com:password").decode("ascii")
-    req.add_header("Authorization", "Basic %s" % userAndPass)
-    with urllib.request.urlopen(req) as f:
-        pass
+        return url_req
 
-    print("Status", f.status)
-    print("Reason", f.reason)
+    def download(self):
+        get_req = urllib.request.Request(
+            url=self.url,
+            method="GET",
+        )
+        get_req.add_header("Authorization", "Basic {}".format(self.auth_b64))
+        with urllib.request.urlopen(get_req) as url_req:
+            file_data = url_req.read()
 
+        with open(self.local_fpath, "wb") as download_f:
+            download_f.write(file_data)
 
-if __name__ == "__main__":
-    https_put()
+        return url_req
